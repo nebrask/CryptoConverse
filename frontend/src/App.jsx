c09914eb0fb4238be47e1180ed95cd340dc55c0d
Nebras Khan <khann84@mcmaster.ca>
2024-07-21 15:21:09 -0400

    Set up page routing for all main components in the application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from './components/ui/button'
-import NavBar from './pages/NavBar/NavBar'
-import Home from './pages/HomeScreen/Home'
+import NavBar from './pages/NavBar/NavBar';
+import Home from './pages/HomeScreen/Home';
+import { Routes, Route } from 'react-router-dom';
+import Portfolio from './pages/PortfolioScreen/Portfolio';
+import Activity from './pages/ActivityScreen/Activity';
+import Wallet from './pages/WalletScreen/Wallet';
+import Withdrawal from './pages/WithdrawalScreen/Withdrawal';
+import Payment from './pages/PaymentScreen/Payment';
+import Watchlist from './pages/WatchlistScreen/Watchlist';
+import Profile from './pages/ProfileScreen/Profile';
+import CryptoDetails from './pages/CryptoDetails/CryptoDetails';
+import SearchCoin from './pages/SearchScreen/SearchCoin';
 
 function App() {
-
   return (
     <>
       <NavBar/>
-      <Home/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/portfolio" element={<Portfolio/>}/>
+        <Route path="/activity" element={<Activity/>}/>
+        <Route path="/wallet" element={<Wallet/>}/>
+        <Route path="/withdrawal" element={<Withdrawal/>}/>
+        <Route path="/payment" element={<Payment/>}/>
+        <Route path="/watchlist" element={<Watchlist/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/market/:id" element={<CryptoDetails/>}/>
+        <Route path="/search" element={<SearchCoin/>}/>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/pages/NavBar/SideBar.jsx
+++ frontend/src/pages/NavBar/SideBar.jsx
@@ -3,10 +3,11 @@ import { Sheet, SheetClose } from '@/components/ui/sheet'
 import { ActivityLogIcon, BookmarkIcon, DashboardIcon, ExitIcon, HomeIcon, PersonIcon } from '@radix-ui/react-icons'
 import { CreditCardIcon, LandmarkIcon, WalletIcon} from "lucide-react"
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const menu = [ 
     {name:"Home", path:"/", icon:<HomeIcon className='h-6 w-6'/>},
-    {name:"Portfolio", path:"/porfolio", icon:<DashboardIcon className='h-6 w-6'/>},
+    {name:"Portfolio", path:"/portfolio", icon:<DashboardIcon className='h-6 w-6'/>},
     {name:"Watchlist", path:"/watchlist", icon:<BookmarkIcon className='h-6 w-6'/>},
     {name:"Activity", path:"/activity", icon:<ActivityLogIcon className='h-6 w-6'/>},
     {name:"Wallet", path:"/wallet", icon:<WalletIcon/>},
@@ -17,12 +18,16 @@ const menu = [
 ]
 
 const SideBar = () => {
+  const navigate = useNavigate();
+  
   return (
     <div className="mt-10 space-y-5">
         { menu.map((item) => (
         <div key={item.name}>
             <SheetClose className='w-full'>
-                <Button variant="outline" className="flex items-center gap-5 py-6 w-full">
+                <Button onClick={()=>navigate(item.path)} 
+                variant="outline" 
+                className="flex items-center gap-5 py-6 w-full">
                     <span className="w-8">
                         {item.icon}
                     </span>
